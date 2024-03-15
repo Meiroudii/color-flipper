2f4cb972ae03015dca3ccf68de9d241233c46a0f
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-03-15 11:44:35 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function colorGreen() {
+    //document.getElementsByTagName("html").style.backgroundColor = "green";
+    document.body.style.backgroundColor = "green";
+    document.getElementById("#headerTitle").style.color = "rbg(34,34,2)";
+
+}
+function colorRed() {
+    document.body.style.backgroundColor = "red";
+}
+function colorBlue() {
+    document.body.style.backgroundColor = "blue";
+}
+function randomColor() {
+    document.body.style.backgroundColor = "random";
+}
